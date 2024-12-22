c59373f85fea818a32125406e2cf181797d2fba2
Sajid820 <sm.sajidur.rahman@g.bracu.ac.bd>
2024-12-22 19:41:59 +0600

    Sprint2 update

---
+++ frontend/src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-light text-center py-3">
+    <p>&copy; 2024 Fitness Tracker. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
